76c18083b96e38c95ca29455c23540eaadea90e7
Mark Ryan <mark.ryan@gmail.com>
2024-01-13 14:44:09 -0700

    fixed input tensor size. other fixes

--- frigate/detectors/plugins/cpu_mp.py
+++ frigate/detectors/plugins/cpu_mp.py
@@ -31,10 +31,18 @@ class CpuMp(DetectionApi):
             base_options=self.BaseOptions(model_asset_path=self.model_path),
             max_results=5,
             running_mode=self.VisionRunningMode.IMAGE,
+            score_threshold=0.2,
         )
 
     def detect_raw(self, tensor_input):
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=tensor_input)
+        tensor_input_reshaped = tensor_input.reshape((720, 1280, 3))
+        # logger.info(f"tensor_input.shape: {tensor_input.shape}")
+        # logger.info(f"tensor_input reshaped {tensor_input_reshaped.shape}")
+
+        mp_image = mp.Image(
+            image_format=mp.ImageFormat.SRGB, data=tensor_input_reshaped
+        )
+        # logger.info(f"mp_image.shape: {mp_image.width}x{mp_image.height}")
 
         with self.ObjectDetector.create_from_options(self.options) as detector:
             detection_result = detector.detect(mp_image).detections
@@ -43,18 +51,20 @@ class CpuMp(DetectionApi):
 
         i = 0
         for detection in detection_result:
-            if any([category.score < 0.4 for category in detection.categories]):
-                break
             logger.info(detection.bounding_box)
             logger.info(detection.categories)
             # TODO: get proper label id
-            x = float(detection.bounding_box.origin_x / tensor_input.shape[1])
-            y = float(detection.bounding_box.origin_y / tensor_input.shape[2])
-            width = float(detection.bounding_box.width / tensor_input.shape[1])
-            height = float(detection.bounding_box.height / tensor_input.shape[2])
+            x = float(detection.bounding_box.origin_x / tensor_input_reshaped.shape[0])
+            y = float(detection.bounding_box.origin_y / tensor_input_reshaped.shape[1])
+            width = float(detection.bounding_box.width / tensor_input_reshaped.shape[0])
+            height = float(
+                detection.bounding_box.height / tensor_input_reshaped.shape[1]
+            )
+            score = float(detection.categories[0].score)
+            # TODO: add proper score below
             detections[i] = [
                 float(1),
-                float(detection.categories[0].score),
+                0.8,
                 x,
                 y,
                 x + width,
