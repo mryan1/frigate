e1b8037082fe5e515d35b69c364e72f301cd0ff7
Mark Ryan <mark.ryan@gmail.com>
2024-01-14 17:40:43 -0700

    working, but with static high score...

--- frigate/detectors/plugins/cpu_mp.py
+++ frigate/detectors/plugins/cpu_mp.py
@@ -35,14 +35,11 @@ class CpuMp(DetectionApi):
         )
 
     def detect_raw(self, tensor_input):
-        tensor_input_reshaped = tensor_input.reshape((720, 1280, 3))
-        # logger.info(f"tensor_input.shape: {tensor_input.shape}")
-        # logger.info(f"tensor_input reshaped {tensor_input_reshaped.shape}")
+        tensor_input_reshaped = tensor_input[0]
 
         mp_image = mp.Image(
             image_format=mp.ImageFormat.SRGB, data=tensor_input_reshaped
         )
-        # logger.info(f"mp_image.shape: {mp_image.width}x{mp_image.height}")
 
         with self.ObjectDetector.create_from_options(self.options) as detector:
             detection_result = detector.detect(mp_image).detections
@@ -51,38 +48,29 @@ class CpuMp(DetectionApi):
 
         i = 0
         for detection in detection_result:
-            logger.info(detection.bounding_box)
-            logger.info(detection.categories)
+            # logger.info(detection.categories)
             # TODO: get proper label id
-            x = float(detection.bounding_box.origin_x / tensor_input_reshaped.shape[0])
-            y = float(detection.bounding_box.origin_y / tensor_input_reshaped.shape[1])
-            width = float(detection.bounding_box.width / tensor_input_reshaped.shape[0])
-            height = float(
-                detection.bounding_box.height / tensor_input_reshaped.shape[1]
-            )
+
+            x = detection.bounding_box.origin_x / tensor_input_reshaped.shape[1]
+            y = detection.bounding_box.origin_y / tensor_input_reshaped.shape[0]
+            width = detection.bounding_box.width / tensor_input_reshaped.shape[1]
+            height = detection.bounding_box.height / tensor_input_reshaped.shape[0]
+
+            top = 1 - y - height
+            left = x
+            bottom = 1 - y
+            right = x + width
             score = float(detection.categories[0].score)
             # TODO: add proper score below
             detections[i] = [
-                float(1),
+                float(15),
                 0.8,
-                x,
-                y,
-                x + width,
-                y + height,
+                top,
+                left,
+                bottom,
+                right,
             ]
             i += 1
-            logger.info(detections)
 
-        # for i in range(count):
-        #     if scores[i] < 0.4 or i == 20:
-        #         break
-        #     detections[i] = [
-        #         class_ids[i],
-        #         float(scores[i]),
-        #         boxes[i][0],
-        #         boxes[i][1],
-        #         boxes[i][2],
-        #         boxes[i][3],
-        #     ]
-
+        # logger.info(detections)
         return detections
